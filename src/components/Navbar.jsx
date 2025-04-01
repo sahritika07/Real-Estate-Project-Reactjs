a3f7b412cc9b9584982b85558d573733710fc8f1
Ritika Sah <ritikasah731@gmail.com>
2025-04-01 22:55:49 +0530

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,13 @@ const Navbar = () => {
       <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32'>
         <img src={assets.logo} alt="" />
         <ul className='hidden md:flex gap-7 text-white' >
-            <a href="#Header" className="cursor-pointer hover:text-gray-400">Home</a>
-            <a href="#About" className="cursor-pointer hover:text-gray-400">About</a>
-            <a href="#Projects" className="cursor-pointer hover:text-gray-400">Projects</a>
-            <a href="#Testimonials" className="cursor-pointer hover:text-gray-400">Testimonials</a>           
+            <a href="#Header" className="cursor-pointer text-lg hover:text-gray-400">Home</a>
+            <a href="#About" className="cursor-pointer text-lg hover:text-gray-400">About</a>
+            <a href="#Projects" className="cursor-pointer text-lg hover:text-gray-400">Projects</a>
+            <a href="#Testimonials" className="cursor-pointer text-lg hover:text-gray-400">Testimonials</a>           
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
+        <img src={assets.menu_icon} className='md:hidden w-7' alt="" />
       </div>
     </div>
   )
