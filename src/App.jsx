8d3d0d97d3eb681ae74c279a3fb3adffdd3015e0
Ritika Sah <ritikasah731@gmail.com>
2025-04-02 22:10:10 +0530

    Created About Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Header from './components/Header.jsx'
+import About from './components/About.jsx'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'> 
     
       <Header/>
+      <About/>
     </div>
   )
 }
