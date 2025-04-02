591d384570b7276ad11a963bd47443794d70541b
Ritika Sah <ritikasah731@gmail.com>
2025-04-02 21:08:17 +0530

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from "../assets/assets"
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
       <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32'>
@@ -13,8 +14,20 @@ const Navbar = () => {
             <a href="#Testimonials" className="cursor-pointer text-lg hover:text-gray-400">Testimonials</a>           
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
-        <img src={assets.menu_icon} className='md:hidden w-7' alt="" />
+        <img onClick={()=> setShowMobileMenu(true)} src={assets.menu_icon} className=' transition-all md:hidden w-7 cursor-pointer' alt="" />
       </div>
+     {/*  --------      Mobile Menu      ------------ */}
+     <div className={`md:hidden ${showMobileMenu? 'fixed w-full' : 'h-0 w-0'}  right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
+      <div className='flex justify-end p-6 cursor-pointer '>
+        <img onClick={()=> setShowMobileMenu(false)} src={assets.cross_icon} className='w-6 ' alt="" />
+      </div>
+      <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium '>
+        <a href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+        <a href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
+        <a href="#Projects" className='px-4 py-2 rounded-full inline-block'>Projects</a>
+        <a href="#Testimonials" className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
+      </ul>
+     </div>
     </div>
   )
 }
