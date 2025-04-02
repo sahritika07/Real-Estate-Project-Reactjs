e637b1b09f5905170dce0d263d45e2555fd61379
Ritika Sah <ritikasah731@gmail.com>
2025-04-02 22:37:34 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Header from './components/Header.jsx'
 import About from './components/About.jsx'
+import Projects from './components/Projects.jsx'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     
       <Header/>
       <About/>
+      <Projects/>
     </div>
   )
 }
