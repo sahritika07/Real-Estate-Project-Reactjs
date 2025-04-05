61d0c833321d70f805d5559cdadec83164cf0a94
Ritika Sah <ritikasah731@gmail.com>
2025-04-05 20:27:48 +0530

    Changes

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='text-center p-6 py-20 lg-px-32 w-full overflow-hidden' id='Contact'>
+      
+    </div>
+  )
+}
+
+export default Contact
