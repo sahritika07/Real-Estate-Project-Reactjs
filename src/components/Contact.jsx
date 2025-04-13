f9f86cfda5ea49133671fbbfc979a879a5f78c18
Ritika Sah <ritikasah731@gmail.com>
2025-04-13 12:32:45 +0530

    Changes in the footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { toast } from 'react-toastify';
+import { motion } from "motion/react"
 
 const Contact = () => {
   const [result, setResult] = React.useState("");
@@ -31,7 +32,10 @@ const Contact = () => {
   };
 
   return (
-    <div className='text-center p-6 py-20 lg-px-32 w-full overflow-hidden' id='Contact'>
+    <motion.div initial={{opacity:0 , x:-200}}
+      transition={{duration:1}} 
+      whileInView={{opacity:1 , x:0}}
+      viewport={{once: true}} className='text-center p-6 py-20 lg-px-32 w-full overflow-hidden' id='Contact'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With Us</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let's Build Future Together</p>
       <form onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-600 pt-8'>
@@ -49,7 +53,7 @@ const Contact = () => {
         </div>
         <button className='bg-blue-600 text-white py-2 px-10 mb-10 rounded'>{result ? result: "Send Message"}</button>
       </form>
-    </div>
+    </motion.div>
   )
 }
 
