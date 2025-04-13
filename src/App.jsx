dcc8c2f3a186cdabcb6e187732aedc8d101e1b9b
Ritika Sah <ritikasah731@gmail.com>
2025-04-13 11:51:26 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import About from './components/About.jsx'
 import Projects from './components/Projects.jsx'
 import Testimonials from './components/Testimonials.jsx'
 import Contact from './components/Contact.jsx'
+import { ToastContainer } from 'react-toastify';
+import Footer from './components/Footer.jsx'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'> 
-       
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
       <Testimonials/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
