1da71503c84d834d35370233c932cad798eec6a9
Ritika Sah <ritikasah731@gmail.com>
2025-04-16 16:59:16 +0530

    Changes in the image width

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
       <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
       <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to your Vision</p>
       <div className="flex flex-col md:flex-row items-center md:items-start md:gap-20">
-          <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg' />
+          <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-md' />
           <div className='flex flex-col items-start md:items-start mt-19 text-gray-600'>
              <div className='grid mt-10 grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
                 <div>
